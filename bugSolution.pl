bf428b70e009368f08fc570972961140c2be8291
BugHunter-X <askzenai@gmail.com>
2025-02-15 23:00:04 +0100

    Add bugSolution.pl file

---
+++ bugSolution.pl
@@ -0,0 +1,7 @@
+my $var = '123';
+my $var2 = '456';
+my $combined = $var . $var2; # String concatenation
+print "Concatenated: $combined\n"; # Output: Concatenated: 123456
+
+my $sum = $var + 0 + $var2 + 0; # Explicit type conversion to numbers
+print "Sum: $sum\n"; # Output: Sum: 579
